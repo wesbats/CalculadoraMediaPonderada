9b4a849578f889d1417fb32e5d4ec4317a9c5b6d
czuy <weslleyvitorbatista@gmail.com>
2023-09-30 17:22:59 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraMédiaPonderada/Aluno.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Pessoa
+{
+    internal class Aluno
+    {
+        private int matricula;
+        private string nome;
+        private double nota1;
+        private double nota2;
+        private double trabalho;
+
+        private double notaFinal => MediaPonderada(nota1, nota2, trabalho);
+        internal string Nome => nome;
+        internal double NotaFinal => notaFinal;
+
+        /// <summary>
+        /// Cria o objeto com todas as informações preenchidas.
+        /// </summary>
+        /// <param name="matricula"></param>
+        /// <param name="nome"></param>
+        /// <param name="nota1"></param>
+        /// <param name="nota2"></param>
+        /// <param name="trabalho"></param>
+        public Aluno(int matricula, string nome, double nota1, double nota2, double trabalho)
+        {
+            this.matricula = matricula;
+            this.nome = nome;
+            this.nota1 = nota1;
+            this.nota2 = nota2;
+            this.trabalho = trabalho;
+        }
+
+        /// <summary>
+        /// Cria o objeto com matricula e nome preenchidos.
+        /// </summary>
+        /// <param name="matricula"></param>
+        /// <param name="nome"></param>
+        public Aluno(int matricula, string nome)
+        {
+            this.matricula = matricula;
+            this.nome = nome;
+        }
+
+        /// <summary>
+        /// Calcula a média final do aluno sem a necessidade de armazenar os dados.
+        /// </summary>
+        /// <param name="nota1"></param>
+        /// <param name="nota2"></param>
+        /// <param name="trabalho"></param>
+        /// <returns></returns>
+        public static double MediaPonderada(double nota1, double nota2, double trabalho) {
+            double notaFinal = double.Parse(((nota1*2.5 + nota2*2.5 + trabalho*2)/(2.5+2.5+2)).ToString("F"));
+            return notaFinal;
+        }
+
+        /// <summary>
+        /// Adiciona nota ao objeto.
+        /// </summary>
+        /// <param name="nota1"></param>
+        /// <param name="nota2"></param>
+        /// <param name="trabalho"></param>
+        internal void AdicionarNota(double nota1, double nota2, double trabalho)
+        {
+            { this.nota1 = nota1; }
+            { this.nota2 = nota2; }
+            { this.trabalho = trabalho; }
+        }
+
+    }
+}
---
+++ CalculadoraMédiaPonderada/Program.cs
@@ -0,0 +1,188 @@
+﻿using Pessoa;
+using System.Linq.Expressions;
+
+internal class Program
+{
+    static void Main()
+    {
+        List<Aluno> alunos = new();
+        bool looping = true;
+        while (looping)
+        {
+            Console.Clear();
+            Console.WriteLine("Selecione uma das opções:");
+            Console.WriteLine("1 - Cadastrar um aluno");
+            Console.WriteLine("2 - Cadastrar nota a um aluno");
+            Console.WriteLine("3 - Cadastrar um aluno com nota");
+            Console.WriteLine("4 - Calcular média");
+            Console.WriteLine("5 - Verificar nota de um aluno");
+            Console.WriteLine("6 - Sair");
+
+            if (int.TryParse(Console.ReadLine(), out int opcaoSelecionada))
+            {
+
+                switch (opcaoSelecionada)
+                {
+                    case 1:
+                        alunos.Add(AlunoSemNota());
+                        break;
+
+                    case 2:
+                        CadastrarNota();
+                        break;
+
+                    case 3:
+                        alunos.Add(AlunoComNota());
+                        break;
+
+                    case 4:
+                        CalcularMedia();
+                        break;
+
+                    case 5:
+                        VerificaNota();
+                        break;
+
+                    case 6:
+                        looping = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida!");
+            }
+        }
+
+        static Aluno AlunoSemNota()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite a matricula: ");
+            int.TryParse(Console.ReadLine(), out int matricula);
+            Console.WriteLine("Digite um nome");
+            string nome = Console.ReadLine();
+
+            Aluno alunos = new Aluno(matricula, nome);
+            Console.WriteLine($"Aluno {nome} cadastrado com sucesso!");
+            return alunos;
+        }
+
+        Aluno? CadastrarNota()
+        {
+            Console.Clear();
+
+            if (alunos.Count > 0)
+            {
+                SelecaoAlunos();
+                Console.WriteLine("Selecione o aluno desejado para cadatrar a nota:");
+
+                if (int.TryParse(Console.ReadLine(), out int selecao) && selecao -1 < alunos.Count)
+                {
+                    Console.Clear();
+
+                    Aluno alunoSelecionado = alunos[selecao - 1];
+                    Console.WriteLine($"Digite as notas para o aluno {alunoSelecionado.Nome}");
+                    Console.WriteLine("Nota da primeira prova: ");
+                    double.TryParse(Console.ReadLine(), out double nota1);
+                    Console.WriteLine("Nota da segunda prova: ");
+                    double.TryParse(Console.ReadLine(), out double nota2);
+                    Console.WriteLine("Nota do trabalho: ");
+                    double.TryParse(Console.ReadLine(), out double trabalho);
+                    alunoSelecionado.AdicionarNota(nota1, nota2, trabalho);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Nota adicionada com sucesso!");
+                    Console.WriteLine($"Média Final: {alunoSelecionado.NotaFinal}");
+                }
+                else
+                {
+                    Console.WriteLine("Opção inválida!");
+                };
+            }
+            else
+            {
+                Console.WriteLine("Sem alunos cadastrados.");
+            }
+
+            Console.ReadKey();
+            return null;
+        }
+
+        Aluno AlunoComNota()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite a matricula: ");
+            int.TryParse(Console.ReadLine(), out int matricula);
+            Console.WriteLine("Digite um nome");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite a nota da primeira prova: ");
+            double.TryParse(Console.ReadLine(), out double nota1);
+            Console.WriteLine("Digite a nota da segunda prova: ");
+            double.TryParse(Console.ReadLine(), out double nota2);
+            Console.WriteLine("Digite a nota do trabalho");
+            double.TryParse(Console.ReadLine(), out double trabalho);
+
+            Aluno alunos = new Aluno(matricula, nome, nota1, nota2, trabalho);
+            Console.WriteLine();
+            Console.WriteLine($"A nota final de {nome} é {alunos.NotaFinal}!");
+            Console.ReadKey();
+            return alunos;
+        }
+        
+        static void CalcularMedia()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite a nota da primeira prova: ");
+            double.TryParse(Console.ReadLine(), out double nota1);
+            Console.WriteLine("Digite a nota da segunda prova: ");
+            double.TryParse(Console.ReadLine(), out double nota2);
+            Console.WriteLine("Digite a nota do trabalho");
+            double.TryParse(Console.ReadLine(), out double trabalho);
+
+            Console.WriteLine();
+            Console.WriteLine($"Nota final: {Aluno.MediaPonderada(nota1, nota2, trabalho)}");
+            Console.ReadKey();
+        }
+        
+        void VerificaNota()
+        {
+            Console.Clear();
+            if (alunos.Count > 0)
+            {
+                SelecaoAlunos();
+                Console.WriteLine("Qual aluno para verificar a nota?");
+
+                if (int.TryParse(Console.ReadLine(), out int selecao) && selecao - 1 < alunos.Count)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Nota final de {alunos[selecao - 1].Nome} é {alunos[selecao - 1].NotaFinal}");
+                }
+                else
+                {
+                    Console.WriteLine("Opção inválida!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Sem alunos cadastrados.");
+            }
+            Console.ReadKey();
+        }
+        
+        Aluno? SelecaoAlunos()
+        {
+            Console.WriteLine("Alunos cadastrados: ");
+            for (int i = 0; i < alunos.Count; i++)
+            {
+                Console.WriteLine($"{i + 1} - {alunos[i].Nome}");
+            }
+            Console.WriteLine();
+            return null;
+        }
+    }
+}
